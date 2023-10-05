2a087198bc4e340744d46569be7c172e28dc4f5c
jessie <jessie.pastan@gmail.com>
2023-10-04 23:30:36 -0400

    Implement dark mode modifier

---
+++ VocBox/Core/Profile/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/4/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileView()
+}
---
+++ VocBox/Helpers/DarkMode.swift
@@ -0,0 +1,8 @@
+//
+//  DarkMode.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/4/23.
+//
+
+import Foundation
