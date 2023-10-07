f62f54199049e690b2b8652a818e34fa6676d314
jessie <jessie.pastan@gmail.com>
2023-10-06 20:48:12 -0400

    implement searchView

---
+++ VocBox/Core/Profile/Search/DefinitionView .swift
@@ -0,0 +1,18 @@
+//
+//  DefinitionView .swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/6/23.
+//
+
+import SwiftUI
+
+struct DefinitionView_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DefinitionView_()
+}
