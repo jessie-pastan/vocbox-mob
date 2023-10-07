04e1c66b8539226596533947b047c92b3d82ee01
jessie <jessie.pastan@gmail.com>
2023-10-07 18:39:12 -0400

    implement customSearchView

---
+++ VocBox/Animation/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/7/23.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
