2ba55e20d411cede69652396d125efc6b13f1621
jessie <jessie.pastan@gmail.com>
2023-10-13 22:12:42 -0400

    Refactor ReadWord function

---
+++ VocBox/Core/Profile/Statistics/TestStatisticView.swift
@@ -0,0 +1,18 @@
+//
+//  TestStatisticView.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/12/23.
+//
+
+import SwiftUI
+
+struct TestStatisticView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestStatisticView()
+}
