54ca9acb58753291c12d79eb6f96835050ef9dc1
jessie <jessie.pastan@gmail.com>
2023-10-20 15:09:46 -0400

    Add custom fonts

---
+++ VocBox/CustomFonts/CustomFont.swift
@@ -0,0 +1,8 @@
+//
+//  CustomFont.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/19/23.
+//
+
+import Foundation
---
+++ VocBox/FontExtension/FontExtention.swift
@@ -0,0 +1,8 @@
+//
+//  FontExtention.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/19/23.
+//
+
+import Foundation
