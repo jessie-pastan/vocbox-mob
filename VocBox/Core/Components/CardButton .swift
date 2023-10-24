9ae5221c037822b2c5fcdc89c1419e01e60a1035
jessie <jessie.pastan@gmail.com>
2023-10-24 14:18:45 -0400

    Refactor buttons

---
+++ VocBox/Core/Components/CardButton .swift
@@ -0,0 +1,18 @@
+//
+//  CardButton .swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/24/23.
+//
+
+import SwiftUI
+
+struct CardButton_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardButton_()
+}
---
+++ VocBox/Core/Components/LongButton.swift
@@ -0,0 +1,18 @@
+//
+//  LongButton.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/24/23.
+//
+
+import SwiftUI
+
+struct LongButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongButton()
+}
