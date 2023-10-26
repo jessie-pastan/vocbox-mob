a6061b87b134c8926b21de3d08c1d12cdb32d26f
jessie <jessie.pastan@gmail.com>
2023-10-25 23:52:57 -0400

    Implement Animation in OnboardingScreens

---
+++ VocBox/Onboarding /LottieView.swift
@@ -0,0 +1,39 @@
+//
+//  LottieView.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/25/23.
+//
+
+import SwiftUI
+import Lottie
+
+struct LottieView: UIViewRepresentable {
+    var name = "defaultName"
+    //var loopMode: LottieLoopMode = .loop
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(name)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+
+        return view
+    }
+
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+    }
+}
+
+
