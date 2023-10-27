cfbf2124d47a3da9e9487c27193f88c861520541
jessie <jessie.pastan@gmail.com>
2023-10-26 23:56:07 -0400

    Implement Reminder function

---
+++ VocBox/Core/Profile/Reminder/ReminderSetting.swift
@@ -0,0 +1,8 @@
+//
+//  ReminderSetting.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/26/23.
+//
+
+import Foundation
---
+++ VocBox/Core/Profile/Reminder/localNotification.swift
@@ -0,0 +1,8 @@
+//
+//  localNotification.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 10/26/23.
+//
+
+import Foundation
