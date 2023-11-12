49617fe426ba4731bc66f27998f28e2d1eea2266
jessie <jessie.pastan@gmail.com>
2023-11-11 22:48:01 -0500

    Implement in-app purchase class Add ProUpgradeView

---
+++ VocBox/Core/ReviewVoc/TestSubscription.swift
@@ -0,0 +1,18 @@
+//
+//  TestSubscription.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/11/23.
+//
+
+import SwiftUI
+
+struct TestSubscription: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestSubscription()
+}
---
+++ VocBox/Subscription/StoreKitManager .swift
@@ -0,0 +1,8 @@
+//
+//  StoreKitManager .swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/11/23.
+//
+
+import Foundation
