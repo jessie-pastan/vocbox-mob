cc4cab81028ee93a3071dfa14254993b65631614
jessie <jessie.pastan@gmail.com>
2023-11-13 13:51:18 -0500

    Add SubScription Button

---
+++ VocBox/Core/Components/SubScriptionButton .swift
@@ -0,0 +1,18 @@
+//
+//  SubScriptionButton .swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/13/23.
+//
+
+import SwiftUI
+
+struct SubScriptionButton_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubScriptionButton_()
+}
