91124ced34479f6e4fe066fc59b033c3e6d8f382
jessie <jessie.pastan@gmail.com>
2023-11-14 11:19:59 -0500

    Update Image in No favorite card view  from systemname  "heart"  to asset "heart"

--- VocBox/Core/Components/CardButton .swift
+++ VocBox/Core/Components/CardButton .swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct CardButton: View {
     
     var title = ""
-    var subtitle = ""
+    
     var body: some View {
         ZStack{
             RoundedRectangle(cornerRadius: 120)
@@ -20,18 +20,12 @@ struct CardButton: View {
                     .font(.custom(.playfairSemibold, size: 17))
                     .font(.body)
                     .bold()
-                    
-                Text(subtitle)
-                    .font(.footnote)
-                    
             }
             .foregroundColor(Color.text)
         }
         .background(Rectangle().foregroundColor(.textField).cornerRadius(120).offset(x: 2, y: 2))
         .padding(.top, 5)
         .padding(.leading, -5)
-        
-        
     }
 }
 
--- VocBox/Core/Components/SubScriptionButton.swift
+++ VocBox/Core/Components/SubScriptionButton.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct SubScriptionButton: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+   
+        var title = ""
+        var subtitle = ""
+        var body: some View {
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 120)
+                    .foregroundColor(Color.button)
+                VStack {
+                    Text(title)
+                        .font(.custom(.playfairSemibold, size: 17))
+                        .font(.body)
+                        .bold()
+                        
+                    Text(subtitle)
+                        .font(.footnote)
+                        
+                }
+                .foregroundColor(Color.text)
+            }
+            .background(Rectangle().foregroundColor(.textField).cornerRadius(120).offset(x: 2, y: 2))
+            .padding(.top, 5)
+            .padding(.leading, -5)
+        }
     }
-}
+
 
 #Preview {
     SubScriptionButton()
