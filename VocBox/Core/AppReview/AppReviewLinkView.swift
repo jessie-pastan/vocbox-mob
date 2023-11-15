5d2c9fdf93418b662e55846946c6db6427dcf814
jessie <jessie.pastan@gmail.com>
2023-11-14 23:50:55 -0500

    Add app review card

---
+++ VocBox/Core/AppReview/AppReviewLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  AppReviewLinkView.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/14/23.
+//
+
+import SwiftUI
+
+struct AppReviewLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppReviewLinkView()
+}
---
+++ VocBox/Core/Components/AppReviewCard .swift
@@ -0,0 +1,18 @@
+//
+//  AppReviewCard .swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/14/23.
+//
+
+import SwiftUI
+
+struct AppReviewCard_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppReviewCard_()
+}
