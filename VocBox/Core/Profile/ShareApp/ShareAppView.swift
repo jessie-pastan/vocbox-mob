c62baa05794db66ce68739afd9d1f59d0572efd5
jessie <jessie.pastan@gmail.com>
2023-11-16 19:33:33 -0500

    Implement Share app  in profile view

---
+++ VocBox/Core/Profile/ShareApp/ShareAppView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareAppView.swift
+//  VocBox
+//
+//  Created by Jessie Pastan on 11/16/23.
+//
+
+import SwiftUI
+
+struct ShareAppView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareAppView()
+}
